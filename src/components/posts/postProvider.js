60a561af1f2c5fd3b97179640c2638e0b3d514e7
Raparker88 <raparker88@gmail.com>
2020-10-22 14:20:12 -0500

    post provider saved

--- src/components/posts/postProvider.js
+++ src/components/posts/postProvider.js
@@ -7,14 +7,14 @@ export const PostProvider = (props) => {
     const [posts, setPosts] = useState([])
 
     const getPosts = () => {
-        return fetch("http://localhost:8088/posts")
+        return fetch("http://localhost:8000/posts")
             .then(res => res.json())
             .then(res => res.sort(compare))
             .then(setPosts)
     }
 
     const addPost = post => {
-        return fetch("http://localhost:8088/posts", {
+        return fetch("http://localhost:8000/posts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
