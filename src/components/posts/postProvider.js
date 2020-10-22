8be0cff5da90fb7272810bf1fad29fb9889b77ee
Raparker88 <raparker88@gmail.com>
2020-10-22 13:50:07 -0500

    created post provider with getPosts and addPost fetch call functions

---
+++ src/components/posts/postProvider.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react"
+
+
+export const PostContext = React.createContext()
+
+export const PostProvider = (props) => {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = () => {
+        return fetch("http://localhost:8088/posts")
+            .then(res => res.json())
+            .then(res => res.sort(compare))
+            .then(setPosts)
+    }
+
+    const addPost = post => {
+        return fetch("http://localhost:8088/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(post)
+        })
+        .then(getPosts) 
+    }
+
+    return (
+        <CropContext.Provider value={{
+            posts, addPost
+        }}>
+            {props.children}
+        </CropContext.Provider>
+    )
+
+}
