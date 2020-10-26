fb777135a7ccb00c37079a809fe5a1095c2801dc
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-10-26 15:29:35 -0500

    tags list rended to page for tags

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -5,6 +5,8 @@ import { CategoryProvider } from "./categories/CategoryProvider"
 import { PostForm } from "./posts/PostForm"
 import { CategoriesList } from "./categories/CategoriesList"
 import { CategoryForm } from "./categories/CategoryForm"
+import { TagProvider } from "./tags/TagsProvider"
+import { TagsList } from "./tags/TagsList"
 
 
 export const ApplicationViews = () => {
@@ -20,15 +22,23 @@ export const ApplicationViews = () => {
                     } />
                 </CategoryProvider>
             </PostProvider>
-        </main>
-        <CategoryProvider>
-            <Route exact path="/categories/create" render ={
-                props => <CategoryForm {...props} />
-            } />
 
-            <Route exact path="/categories" render={
-                props => <CategoriesList {...props} />
-            } />
-        </CategoryProvider>
+            <CategoryProvider>
+                <Route exact path="/categories/create" render={
+                    props => <CategoryForm {...props} />
+                } />
+
+                <Route exact path="/categories" render={
+                    props => <CategoriesList {...props} />
+                } />
+            </CategoryProvider>
+
+            <TagProvider>
+                <Route exact path="/tags" render={
+                    props => <TagsList {...props} />
+                } />
+            </TagProvider>
+        </main>
+
     </>
 }
