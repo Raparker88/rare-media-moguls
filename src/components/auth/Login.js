074bd5a810a76e2c0819991a62f1ccc3756e39f6
heymonicakay <hey.monicakay@gmail.com>
2020-10-27 10:26:01 -0500

    Updated all instanced of path route '/home' to path route '/' and deleted unused project boilerplate files

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -23,7 +23,7 @@ export const Login = (props) => {
             .then(exists => {
                 if (exists && exists.password === password.current.value) {
                     localStorage.setItem("rare_user_id", exists.id)
-                    props.history.push("/home")
+                    props.history.push("/")
                 } else if (exists && exists.password !== password.current.value) {
                     invalidDialog.current.showModal()
                 } else {
