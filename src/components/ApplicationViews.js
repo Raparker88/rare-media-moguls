fb4938c930e22bae8290a11e12d0d950f51d1e85
heymonicakay <hey.monicakay@gmail.com>
2020-10-27 06:42:10 -0500

    Created user navbar per wireframe design with functional links to view and create posts, manage categories and tags, and logout.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,21 @@
 import React from "react"
+import { Nav } from "./nav/Nav"
 import { Route } from "react-router-dom"
+import "./Rare.css"
 
-export const ApplicationViews = () => {
-    return <>
-        <main style={{
-            margin: "5rem 2rem",
+export const ApplicationViews = (props) => {
+    return (
+    <>
+        <main className="main-container" style={{
+            margin: "0 0",
             lineHeight: "1.75rem"
         }}>
+            <Route path = "/" render = {props =>
+                <nav className="cont--nav">
+                    <Nav {...props} />
+                </nav>
+            }/>
         </main>
     </>
+    )
 }
--- src/components/Rare.js
+++ src/components/Rare.js
@@ -1,36 +1,42 @@
 import React from "react"
 import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
-import { Nav } from "./nav/Nav"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
+import "./Rare.css"
 
 export const Rare = () => (
     <>
         <Route render={() => {
             if (localStorage.getItem("rare_user_id")) {
-                return <>
-                    <Nav />
-                    <ApplicationViews />
-                </>
-            } else {
+
+                return (
+                    <>
+                    <Route render={props =>
+                        <ApplicationViews
+                        {...props}  />} />
+                    </>
+                )
+            }
+            else {
                 return <Redirect to="/login" />
             }
         }} />
 
-        <Route path="/login" render={() => {
+        <Route path="/login" render={(props) => {
             if (localStorage.getItem("rare_user_id")) {
                 return <Redirect to="/" />
             } else {
-                return <Login />
+                return <Login {...props} />
             }
         }} />
 
-        <Route path="/register" render={() => {
+        <Route path="/register" render={(props) => {
             if (localStorage.getItem("rare_user_id")) {
                 return <Redirect to="/" />
-            } else {
-                return <Register />
+            }
+            else {
+                return <Register {...props}/>
             }
         }} />
     </>
