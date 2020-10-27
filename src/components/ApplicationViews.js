0abba3692e34240b13354afab8817eb80d796205
Connor Blakeney <connorblakeney@yahoo.com>
2020-10-27 14:22:09 -0500

    removed extra imports

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Nav } from "./nav/Nav";
 import { Route } from "react-router-dom";
 import "./Rare.css";
@@ -31,59 +31,28 @@ export const ApplicationViews = (props) => {
                     <Route exact path="/new_post" render={(props) =>
                         <PostForm {...props} />}
                     />
-                    <Route exact path="/" render={
-                        props => <PostList {...props} />
-                    } />
-                </CategoryProvider>
-            </PostProvider>
-
-            <CategoryProvider>
-                <Route exact path="/categories/create" render={(props) =>
-                    <CategoryForm {...props} />}
-                />
-            </CategoryProvider>
-
-            <CategoryProvider>
-                <Route exact path="/" render={(props) => (
-                    <>
-                        <div className="main-wrap">
-                            <div className="top-spacer"></div>
-                            <div className="mid-section">
-                                <div className="left-main"></div>
-                                <div className="divider"></div>
-                                <div className="right-main">
-                                    <CategoryButtonList
-                                    {...props} />
+                    <Route exact path="/" render={(props) => (
+                        <>
+                            <PostList {...props}></PostList>
+                            <div className="main-wrap">
+                                <div className="top-spacer"></div>
+                                <div className="mid-section">
+                                    <div className="left-main"></div>
+                                    <div className="divider"></div>
+                                    <div className="right-main">
+                                        <CategoryButtonList
+                                        {...props} />
+                                    </div>
                                 </div>
+                                <div className="bottom-spacer"></div>
                             </div>
-                            <div className="bottom-spacer"></div>
-                        </div>
-                    </>
-                )}/>
-            </CategoryProvider>
-
-            <PostProvider>
-                <CategoryProvider>
-                    <Route exact path="/new_post" render={(props) =>
-                        <PostForm {...props} />}
-                    />
+                        </>
+                    )}/>
                 </CategoryProvider>
             </PostProvider>
 
             <PostProvider>
                 <Route path="/posts/:postId(\d+)" render={(props) =>
-                    <PostDetails {...props} />}
-                />
-            </PostProvider>
-
-            <CategoryProvider>
-                <Route exact path="/categories/create" render={(props) =>
-                    <CategoryForm {...props} />}
-                />
-            </CategoryProvider>
-
-            <PostProvider>
-                <Route path="/posts/:postId(\d+)" render={props =>
                     <PostDetails {...props} />}
                 />
             </PostProvider>
