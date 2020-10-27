fb5efdac5efb49955a15c166980ac5a76eb85410
heymonicakay <hey.monicakay@gmail.com>
2020-10-27 14:51:00 -0500

    Users can create new tags via the tagform on the tagslist page.

--- src/components/tags/TagsList.js
+++ src/components/tags/TagsList.js
@@ -1,9 +1,12 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Tag } from "./Tag"
 import { TagContext } from "./TagProvider"
+import { TagForm } from "./TagForm"
+import "./Tag.css"
 
 export const TagsList = (props) => {
     const {tags, getTags} = useContext(TagContext)
+    const [isHidden, setIsHidden] = useState(true)
 
     useEffect(() => {
         getTags()
@@ -13,14 +16,19 @@ export const TagsList = (props) => {
         <>
             <section className="tags">
 
-                <button onClick={() => props.history.push("/tags/create")}>
+                <TagForm
+                isHidden={isHidden}
+                setIsHidden={setIsHidden}
+                {...props} />
+                <button onClick={()=>{
+                    setIsHidden(false)}}>
                     Add New Tag
                 </button>
 
                {
                    tags.map(tag => {
                     return <Tag key={tag.id} tag={tag} {...props} />
-                   })
+                   }).reverse()
                }
            </section>
         </>
