b127839d32236a43bd5f90b11cc37a32ddcbfa69
heymonicakay <hey.monicakay@gmail.com>
2020-11-11 14:28:57 -0600

    Refactored login to set local storage with logged in user_id and logged in user's auth token.

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -3,62 +3,79 @@ import { Link } from "react-router-dom"
 import "./Auth.css"
 
 export const Login = (props) => {
-    const email = useRef(null)
+    const username = useRef(null)
     const password = useRef(null)
     const invalidDialog = useRef(null)
 
-    const existingUserCheck = () => {
-        return fetch(`http://localhost:8000/users?email=${email.current.value}`)
-            .then(res => {
-                return res.json()
-            })
-            .then(user => {
-                return user.id !== 0 ? user : false
-            })
-    }
-
     const handleLogin = (e) => {
-        e.preventDefault()
-        existingUserCheck()
-            .then(exists => {
-                if (exists && exists.password === password.current.value) {
-                    localStorage.setItem("rare_user_id", exists.id)
-                    props.history.push("/")
-                } else if (exists && exists.password !== password.current.value) {
-                    invalidDialog.current.showModal()
-                } else {
-                    invalidDialog.current.showModal()
-                }
+        e.preventDefault();
+
+        return fetch("http://localhost:8000/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                username: username.current.value,
+                password: password.current.value,
             })
-    }
+        })
+            .then(res => res.json())
+            .then(res => {
+                if ("valid" in res && res.valid && "token" in res) {
+                    localStorage.setItem("rare_token", res.token)
+                    localStorage.setItem("rare_user_id", res.user_id)
+                    console.log(res, "RESPONSE")
+                    props.history.push("/");
+            }
+            else {
+                invalidDialog.current.showModal();
+            }
+            });
+        };
 
     return (
         <main className="container--login">
             <dialog className="dialog dialog--auth" ref={invalidDialog}>
-                <div>Email or password was not valid.</div>
+                <div>Username or password was not valid.</div>
                 <button className="button--close" onClick={e => invalidDialog.current.close()}>Close</button>
             </dialog>
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Rare</h1>
+                    <h1>Media Moguls</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
-                        <label htmlFor="inputEmail"> Email address </label>
-                        <input ref={email} type="email" id="email" className="form-control" defaultValue="me@me.com" placeholder="Email address" required autoFocus />
+                        <label htmlFor="username"> Username </label>
+                        <input
+                            ref={username}
+                            type="text"
+                            id="username"
+                            className="form-control"
+                            defaultValue=""
+                            placeholder="Username"
+                            required
+                            autoFocus />
                     </fieldset>
                     <fieldset>
                         <label htmlFor="inputPassword"> Password </label>
-                        <input ref={password} type="password" id="password" className="form-control" defaultValue="me" placeholder="Password" required />
+                        <input ref={password}
+                            type="password"
+                            id="password"
+                            className="form-control"
+                            defaultValue="me"
+                            placeholder="Password"
+                            required />
                     </fieldset>
                     <fieldset style={{
                         textAlign:"center"
                     }}>
-                        <button className="btn btn-1 btn-sep icon-send" type="submit">Sign In</button>
+                        <button className="btn btn-1 btn-sep icon-send" type="submit">Login</button>
                     </fieldset>
                 </form>
             </section>
             <section className="link--register">
-                <Link to="/register">Not a member yet?</Link>
+                <Link to="/register">Don't have an account yet? Click here to sign up!</Link>
             </section>
         </main>
     )
