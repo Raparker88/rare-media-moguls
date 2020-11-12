11c63c3c5161e02c19c6f44f278ed9f3a00ebe25
heymonicakay <hey.monicakay@gmail.com>
2020-11-12 15:01:18 -0600

    can now edit tags

---
+++ src/components/tags/EditTagForm.js
@@ -0,0 +1,55 @@
+import React, { useContext, useState } from "react"
+import { TagContext } from "./TagProvider"
+import "./Tag.css"
+
+export const EditTagForm = (props) => {
+    const { updateTag, getTags } = useContext(TagContext)
+
+    const [tag, setTag] = useState({})
+    console.log(props.tagToBeEdited, "tag to be edited")
+
+    const handleControlledInputChange = (e) => {
+        const newTag = Object.assign({}, tag)
+        newTag[e.target.name] = e.target.value
+        setTag(newTag)
+    }
+
+    const changeTag = () => {
+        const newTagObject = {
+            id: props.tagToBeEdited.id,
+            label: tag.label
+        }
+        updateTag(newTagObject)
+            .then(()=> {
+                props.setEditMode(false)
+                props.setTagToBeEdited({})
+                getTags()
+            })
+
+        }
+
+    return (
+
+        <form className="form change_tag_form" id="editTagForm">
+            <h2 className="tagForm_label">Edit this tag</h2>
+            <fieldset>
+                <div className="form-div">
+                    <input type="text" name="label" className="form-control edit-tag-input" id="label"
+                        proptype="varchar"
+                        defaultValue={props.tagToBeEdited.label}
+                        onChange={handleControlledInputChange}>
+                    </input>
+                </div>
+            </fieldset>
+            <button type="submit"
+                onClick={e => {
+                    e.preventDefault()
+                    changeTag()
+                }}
+                className="btn post_submit_btn">
+                Save Tag
+            </button>
+
+        </form>
+    )
+}
