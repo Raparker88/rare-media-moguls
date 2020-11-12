b35714d9ba2ae88c7f0dc8d636141cda36d46fd5
Connor Blakeney <connorblakeney@yahoo.com>
2020-11-12 15:37:45 -0600

    removed commented lines from user post list

--- src/components/posts/UserPostList.js
+++ src/components/posts/UserPostList.js
@@ -57,7 +57,6 @@ export const UserPostList = (props) => {
                                 deletePost={deletePost}
                                 setAreYouSure={setAreYouSure}
                                 areYouSure={areYouSure}
-                                // currentUserId={currentUserId}
                                 {...props} />
                         })
                     : null
