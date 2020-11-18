5078d9c3c49751fc989442f3e1e758b85789c279
heymonicakay <hey.monicakay@gmail.com>
2020-11-18 10:21:49 -0600

    added subscribe button to user profile

---
+++ src/components/users/SubscribeButton.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./User.css"
+
+export const SubscribeButton = (props) => {
+
+    return (
+        <button className="subscribe btn" onClick={()=>{
+            // conditional logic for subscribe and unsubscribe
+            return
+        }}>
+            {/* turnery statement for subscribe and unsubscribe */}
+            Subscribe
+        </button>
+    )
+}
--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -3,90 +3,112 @@ import DefaultProfileImage from "./default_profile_image.jpg"
 import { Link } from "react-router-dom"
 import { UserContext } from "./UserProvider"
 import { PostContext } from "../posts/PostProvider"
+import { SubscribeButton } from "./SubscribeButton"
+import "./User.css"
 
 export const UserProfile = (props) => {
     const { getUserProfile } = useContext(UserContext)
+    const { getPosts, posts } = useContext(PostContext)
     const [profile, setProfile] = useState({})
     const [image, setImage] = useState(DefaultProfileImage)
     const [DD, setDD] = useState("00")
     const [MM, setMM] = useState("00")
     const [YYYY, setYYYY] = useState("0000")
-    const [articleCount, setArticleCount] = useState(0)
+    const [postCount, setPostCount] = useState(0)
+    const [plural, setPlural] = useState("")
 
     useEffect(() => {
         const userId = parseInt(props.match.params.userId)
         getUserProfile(userId)
         .then(setProfile)
+        .then(getPosts)
     }, [])
 
+    useEffect(()=>{
+        if(profile !== {} && posts !== []){
+            const count = posts.filter(p => p.rareuser.id === profile.id).length
+            setPostCount(count)
+        }
+    },[posts])
+
+    useEffect(()=>{
+        if(postCount === 0){
+            setPostCount(null)
+        }
+        if(postCount === 1){
+            setPlural("post")
+        }
+        if(postCount > 1){
+            setPlural("posts")
+        }
+    })
 
     useEffect(()=> {
-        if(profile !== {}){
-            if(profile.date_joined !== undefined){
-                const date = profile.date_joined.split('T')[0]
-                const [year, month, day] = date.split('-')
-                const [dayOne, dayTwo] = day.split('')
-                const [monthOne, monthTwo] = month.split('')
-                if(dayOne === "0"){
-                    setDD(dayTwo)
-                }
-                else{
-                    setDD(day)
-                }
-                if(monthOne === "0"){
-                    setMM(monthTwo)
-                }
-                else {
-                    setMM(month)
-                }
-                setYYYY(year)
+        if(profile !== {} && profile.date_joined !== undefined){
+            const date = profile.date_joined.split('T')[0]
+            const [year, month, day] = date.split('-')
+            const [dayOne, dayTwo] = day.split('')
+            const [monthOne, monthTwo] = month.split('')
+            if(dayOne === "0"){
+                setDD(dayTwo)
             }
-            if(profile.profile_image_url !== null){
-                const img = profile.profile_image_url
-                setImage(img)
+            else{
+                setDD(day)
             }
+            if(monthOne === "0"){
+                setMM(monthTwo)
+            }
+            else {
+                setMM(month)
+            }
+            setYYYY(year)
         }
     }, [profile])
 
-    console.log(DD, "DAY OUTSIDE USE EFFECT")
-    console.log(MM, "MONTH OUTSIDE USE EFFECT")
-    console.log(YYYY, "MONTH OUTSIDE USE EFFECT")
-
     return (
         <article className="profile">
-            <section className="profile__info-left">
-                <div className="profile__img">
-                    <img className="image" src={image} />
-                </div>
-                <div className="profile__name">
-                    {profile.full_name}
-                </div>
-            </section>
+            <SubscribeButton {...props} />
+            <div className="top">
+                <section className="profile__info-left">
+                    <div className="profile__img">
+                        <img className="image" src={image} />
+                    </div>
+                    <div className="profile__name">
+                        {profile.full_name}
+                    </div>
+                </section>
 
-            <section className="profile__info-right">
-                <div className="profile__username">
-                    @{profile.username}
-                </div>
-                <div className="profile__email">
-                    Email: {profile.email}
-                </div>
-                <div className="profile__datejoin">
-                    Member Since: {MM}-{DD}-{YYYY}
-                </div>
-                <div className="profile__type">
-                    {profile.is_staff
-                    ? "Admin"
-                    : "Author"
+                <section className="profile__info-right">
+                    <div className="profile__username">
+                        @{profile.username}
+                    </div>
+                    <div className="profile__email">
+                        <p className="email">
+                        {profile.email}
+                        </p>
+                    </div>
+                    <div className="profile__datejoin">
+                        <p className="mem-since">
+                        member since: {MM}-{DD}-{YYYY}
+                        </p>
+                    </div>
+                    <div className="profile__type">
+                        {profile.is_staff
+                        ? "admin"
+                        : "author"
                     }
-                </div>
-                <Link
-                title={`Article Count ${articleCount}`}
-                className="profile__articles"
-                to={`/users/posts`} >
-                    {articleCount}
-                </Link>
-            </section>
-                <div className="profile__bio">Bio:{profile.bio}</div>
+                    </div>
+                    <Link
+                    title={`Post Count: ${postCount}`}
+                    className="profile__articles"
+                    to={`/users/posts`} >
+                        {postCount} {plural}
+                    </Link>
+                </section>
+            </div>
+            <div className="bottom profile__bio">
+                {profile.bio}
+            </div>
         </article>
     )
 }
