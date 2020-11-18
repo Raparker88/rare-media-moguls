3c25d008c4c579d41b348bf58ba4cf33395a6492
Austin Phy <atphy42@gmail.com>
2020-11-18 14:49:16 -0600

    started on posts list

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -19,26 +19,27 @@ export const PostList = (props) => {
 
     return (
         <>
-        <div className="mainPostContainer">
-            <h2>Posts</h2>
-            <button
+        <h2 className="posts-site-title">Rare</h2>
+                    <button
                     className="btn newPostbtn"
                     onClick={() => {
                         props.history.push(`/new_post/`)
                         window.location.reload()
-                    }}>Create New Post</button>
+                    }}>Add Post +</button>
+        <div className="mainPostContainer">
             {
                 posts !== [] ?  
                     currentUser.is_staff === true ?
                         posts.map(p => { 
-                        return <div key={p.id}>
-                        <div className="post-author">
-                            <p>{p.rareuser.full_name}</p>
-                            <p style={{ marginLeft: '.5rem' }} >• {new Date(p.publication_date).toDateString()}</p>
-                        </div>
+                        return <div className="post-list-single" key={p.id}>
                         <Link className="postLink" to={{pathname:`/posts/${p.id}`}}>
-                        <p>{p.title}</p>
+                            <p>{p.title}</p>
                         </Link>
+                        <p style={{ marginLeft: '.5rem' }} className="publication-date">Publication Date {new Date(p.publication_date).toDateString()}</p>
+                        <img className="post-img" src="https://via.placeholder.com/300x150.png"></img>
+                        <div className="post-author">
+                            <p>Author: {p.rareuser.full_name}</p>
+                        </div>
                         <p>Posted in <b>{p.category.label}</b></p>
                         <AdminPostApproval post = {p}/>
                         </div>
