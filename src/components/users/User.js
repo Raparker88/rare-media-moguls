e030a676fecf8dffc2618c7f0a8e8bedae60534c
Connor Blakeney <connorblakeney@yahoo.com>
2020-11-19 14:16:54 -0600

    added prompt to user, trying to make styling match theme better

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -7,17 +7,47 @@ export const User = (props) => {
 
     const { changeUserType, changeUserActive } = useContext(UserContext)
 
-    const statusPrompt = (id) => {
-        let prompt = window.confirm("Are you sure you want to change this user's account status?");
-        if( prompt === true ) {
-            changeUserActive(id)
-            return true;
-        } else {
-            return false;
-        }
-    }
+    // const statusPrompt = (id) => {
+    //     let prompt = window.confirm("Are you sure you want to change this user's account status?");
+    //     if( prompt === true ) {
+    //         changeUserActive(id)
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 
-    return (
+        const statusPrompt = (props, id) => {
+
+            return (
+                <form className="form change_tag_form" id="editTagForm">
+                    <div className="toprow">
+                        <div className="toprowblank"></div>
+                        <span className="x" onClick={()=>{
+                            props.setDeleteMode(false)
+                        }}>X</span>
+                    </div>
+                    <h4 className="tagForm_label">Are you sure you want to delete this Tag?</h4>
+
+                    <button type="submit"
+                        onClick={e => {
+                            changeUserActive(id)
+                            e.preventDefault()
+                        }}
+                        className="btn post_submit_btn">
+                        Ok
+                    </button>
+                    <button type="button"
+                        className="btn cancel"
+                        onClick={e => {
+                            e.preventDefault()
+                            props.setDeleteMode(false)
+                        }}>
+                            Cancel
+                    </button>
+                </form>
+                )}
+            return (
         <>
 
             <tr key={props.user.id}>
@@ -34,7 +64,7 @@ export const User = (props) => {
                     </Link>
                 </td>
                 <td className="userInfo"><label>
-                        <input type="checkbox" id="userRadio" checked={props.user.is_active} onChange={() => statusPrompt(props.user.id)} ></input>
+                        <input type="checkbox" id="userRadio" checked={props.user.is_active} onChange={() => statusPrompt(props, props.user.id)} ></input>
                         Active
                     </label>
                 </td>
